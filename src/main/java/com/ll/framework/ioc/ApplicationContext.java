0669ca1aa8ffd7238821fff0c25925827195141e
Sinou3936 <usa3707@naver.com>
2026-04-22 16:45:33 +0900

    t4 GREEN

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -11,15 +11,18 @@ public class ApplicationContext {
 
     private Map<String, Object> beans = new HashMap<>();
     private final TestPostService testPostService;
+    private final TestPostRepository testPostRepository;
 
     public ApplicationContext() {
         this.testPostService = new TestPostService();
         beans.put("testPostService",testPostService);
-
+        this.testPostRepository = new TestPostRepository();
     }
 
     public <T> T genBean(String beanName) {
-        if("testPostService".equalsIgnoreCase(beanName))
+        if("testPostRepository".equalsIgnoreCase(beanName))
+            return (T) testPostRepository;
+        else if("testPostService".equalsIgnoreCase(beanName))
             return (T) beans.get(beanName);
         return null;
 
