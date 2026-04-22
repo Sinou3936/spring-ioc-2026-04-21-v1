8122a8dce0483efa18c553c23ce9da4f673f8235
Sinou3936 <usa3707@naver.com>
2026-04-22 14:38:48 +0900

    t1 GREEN

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,16 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationContext {
+
     public ApplicationContext() {
 
     }
 
-    public <T> T genBean(String beanName) {
-        return (T) null;
-    }
+    public <T> T genBean(String beanName) { return (T) null;}
 }
