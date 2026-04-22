829762f8c3f0d6848dab429504b6746d76c7e9a3
Sinou3936 <usa3707@naver.com>
2026-04-22 16:40:20 +0900

    t3 GREEN, Blue

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,5 +1,6 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
@@ -11,12 +12,13 @@ public class ApplicationContext {
     private Map<String, Object> beans = new HashMap<>();
     private final TestPostService testPostService;
 
+
     public ApplicationContext() {
         this.testPostService = new TestPostService();
         beans.put("testPostService",testPostService);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) beans.get(beanName);
+        return (T)beans.get(beanName);
     }
 }
