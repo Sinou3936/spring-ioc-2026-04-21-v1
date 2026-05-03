6b9869d9dc1848468eac816ef7974d3e89ac2475
yunabin <yhb1020525@gmail.com>
2026-05-03 20:32:46 +0900

    feat: genBean 생성

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,28 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationContext {
+    private final Map<String, Object> beanContainer = new HashMap<>();
+
     public ApplicationContext() {
+        TestPostRepository testPostRepository = new TestPostRepository();
 
+        TestPostService testPostService = new TestPostService(testPostRepository);
+
+        TestFacadePostService testFacadePostService =
+                new TestFacadePostService(testPostService, testPostRepository);
+
+        beanContainer.put("testPostRepository", testPostRepository);
+        beanContainer.put("testPostService", testPostService);
+        beanContainer.put("testFacadePostService", testFacadePostService);
     }
-
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) beanContainer.get(beanName);
     }
 }
